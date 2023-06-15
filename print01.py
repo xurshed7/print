f71285187c0490c7b94d26e58fab5c7795957ef5
xurshed7 <pandorra.game@gmail.com>
2023-06-15 13:33:49 +0500

    HomeWork

--- print01.py
+++ print01.py
@@ -1,4 +1,3 @@
-
 print('  )')
 print(' (')
 print('[_])')
---
+++ print17.py
@@ -0,0 +1,8 @@
+print('    /\    ')
+print('   /  \   ')
+print('  /    \  ')
+print(' /______\ ')
+print('|        |')
+print('|        |')
+print('|        |')
+print('|________|')
